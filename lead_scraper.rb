58fd9ac657ea1c3b5eb3cc3df0dc368e676ca5a3
Jon Glass <jonglass82@gmail.com>
2018-06-21 15:20:29 -0500

    initial commit

---
+++ lead_scraper.rb
@@ -0,0 +1,28 @@
+require 'open-uri'
+require 'nokogiri'
+require 'erb'
+
+puts "running scraper ..."
+
+
+url = 
+
+puts data = Nokogiri::HTML(open(url))
+
+puts data.at_css('').text
+
+
+class Scraper
+    
+    attr_accessor :title
+
+def initialize (title)
+    
+    @title = "title"
+
+end
+    
+end
+
+
+        
