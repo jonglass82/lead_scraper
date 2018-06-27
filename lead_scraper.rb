8e7d97d8156b25acb5797a8beab88f6c984affc0
Jon Glass <jonglass82@gmail.com>
2018-06-27 16:40:29 -0500

    added zip code prompt and worked on looping through listing info

--- lead_scraper.rb
+++ lead_scraper.rb
@@ -1,28 +1,36 @@
 require 'open-uri'
 require 'nokogiri'
-require 'erb'
-
-puts "running scraper ..."
 
 
-url = ""
-
-puts data = Nokogiri::HTML(open(url))
-
-puts data.at_css('').text
-
-
-class Scraper
+class Scraper 
     
-    attr_accessor :title
-
-def initialize (title)
     
-    @title = "title"
+puts "Please enter a zip code to search ..."
+
+zip_code = gets.chomp
+
+url = "https://www.yogafinder.com/yoga.cfm?Yogacity=" + zip_code.to_s
+
+data = Nokogiri::HTML(open(url))
+    
+
+#loop through all studio listings
+    
+data.css('.maininfo2').each do |studio_info|
+        
+    #studio_info = data.css('.insidetext').children.text 
+    
+    puts data.css('.insidetext').children.text 
+   
+    #puts studio_info
+  
+end
 
 end
+
+#def create_csv
     
-end
+#end
 
 
         
